edaf9568cad258c8d36b5225ed7feaa595734a5e
EdilsonMaria <edilson.eng1@gmail.com>
2023-03-21 21:48:00 -0300

    1º Atualização

---
+++ Aula03 - Exercicio.py
@@ -0,0 +1,20 @@
+# Crie um programa em Python que imprima uma mensagem que você ama programar com esta linguagem.
+
+nome = 'Olá, tubo bem? Como voce esta?'
+print(nome)
+
+#Crie um programa em Python que pergunte o nome do usuário e imprima um bom dia com o nome do
+
+nome = input('Por favor, digite seu nome:  ')
+print(f'Olá, sr. {nome}, Bom dia!')
+
+# Crie um programa em Python que solicite um número do usuário, depois some este número com
+
+
+numero = float(input('Por favor, digite seu numero:  '))
+numero = numero + 1357
+numero = numero * 8
+numero = numero / 5
+numero = numero ** 2
+print(numero)
+
